d4d42437da2dfb918b7152cb8445ebe8868b3351
Guillaume FRUTOSO <gfrutoso@index-education.com>
2016-02-15 14:47:54 +0100

    test javascript

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function sendmessage() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-     document.getElementById("demo").innerHTML = xhttp.responseText;
+        var x = document.querySelectorAll("article.favoris");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.visible = "hidden";
+        }
     }
   };
   xhttp.open("GET", "/minisab/miniflux.cgi?testjava", true);
