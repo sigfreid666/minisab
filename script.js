645d2824497d80061574300d79c3c3eac251b101
GRIMMY <admin@GRIMMY>
2016-02-15 15:09:52 +0100

    Le javascript marche'

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function sendmessage() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
         var x = document.querySelectorAll("article.favoris");
         for (i = 0; i < x.length; i++) {
-            x[i].style.visible = "hidden";
+            x[i].style.visiblity = "hidden";
+            x[i].style.display = "none";
         }
     }
   };
