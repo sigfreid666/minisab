e911ad1ee3a8b45957ed7efdced5d818a7d645c6
Guillaume Frutoso <sigfreids@gmail.com>
2017-07-23 16:21:18 +0200

    Formatage de code

--- flaskminisab.py
+++ flaskminisab.py
@@ -9,23 +9,26 @@ from settings import host_redis, port_redis, host_sabG, sabnzbd_nc_cle_api
 
 
 version = '2.4a'
-bp = Blueprint('minisab', __name__, static_url_path='/minisab/static', static_folder='static')
+bp = Blueprint('minisab', __name__, static_url_path='/minisab/static',
+               static_folder='static')
 
-categorie_sabnzbd = []
+nom_cat_sab = 'minisab_categorie_sabnzbd'
 
 
 @bp.route("/")
 def index():
     articles, favoris = newminisab.recuperer_tous_articles_par_categorie()
-    newminisab.logger.info('index, articles %d, favoris %d', len(articles), len(favoris))
+    newminisab.logger.debug('index, articles %d, favoris %d',
+                            len(articles), len(favoris))
     status_sab = status_sabnzbd()
-    newminisab.logger.info('index, statut sab %s', str(status_sab))
+    newminisab.logger.debug('index, statut sab %s', str(status_sab))
     for x in favoris:
         for y in x.recherche:
-            newminisab.logger.info('index, title %s, id sab %s', x.title, y.id_sabnzbd)
+            newminisab.logger.debug('index, title %s, id sab %s',
+                                    x.title, y.id_sabnzbd)
             if y.id_sabnzbd in status_sab:
                 x.status_sabnzbd = status_sab[y.id_sabnzbd]
-                newminisab.logger.info('index, trouve %s', x.status_sabnzbd)
+                newminisab.logger.debug('index, trouve %s', x.status_sabnzbd)
     articles = ([(x[0].nom, len(x[1]), x[1], x[0].id)
                  for x in articles.items()])
     return render_template('./minifluxlist.html', titlepage='Miniflux',
@@ -40,7 +43,8 @@ def marquer_article_favoris(id_article=None):
         ar = newminisab.article.get(newminisab.article.id == id_article)
         ar.favorie = True
         ar.lancer_recherche()
-        newminisab.logger.info('marquer favoris %d nb recherche %d', id_article, len(ar.recherche))
+        newminisab.logger.info('marquer favoris %d nb recherche %d',
+                               id_article, len(ar.recherche))
         ar.save()
         return render_template('./article.html', item=ar,
                                categorie_sabnzbd=get_categorie_sabnzbd())
@@ -57,7 +61,8 @@ def marquer_article_lu():
                 ar = newminisab.article.get(newminisab.article.id == art_id)
                 ar.lu = True
                 for y in ar.recherche:
-                    newminisab.logger.info('article_lu, title %s, id sab %s', ar.title, y.id_sabnzbd)
+                    newminisab.logger.info('article_lu, title %s, id sab %s',
+                                           ar.title, y.id_sabnzbd)
                     if y.id_sabnzbd != '':
                         delete_history_sab(y.id_sabnzbd)
                 ar.save()
@@ -81,11 +86,13 @@ def recherche_article(id_article, stop_multi):
 
 
 @bp.route('/recherche/<id_recherche>/telecharger/<categorie>')
-@bp.route('/recherche/<id_recherche>/telecharger/', defaults={'categorie': '*'})
+@bp.route('/recherche/<id_recherche>/telecharger/',
+          defaults={'categorie': '*'})
 def lancer_telecharger(id_recherche, categorie):
     try:
         rec = newminisab.recherche.get(newminisab.recherche.id == id_recherche)
-        rec.id_sabnzbd = telechargement_sabnzbd(rec.article.title, rec.url, categorie)
+        rec.id_sabnzbd = telechargement_sabnzbd(rec.article.title,
+                                                rec.url, categorie)
         rec.save()
         return 'OK'
     except newminisab.article.DoesNotExist:
@@ -94,7 +101,9 @@ def lancer_telecharger(id_recherche, categorie):
 
 @bp.route('/categorie/<str_categorie>/lu')
 def categorie_lu(str_categorie=None):
-    cat = newminisab.article.select().where(newminisab.article.categorie == str_categorie)
+    cat = (newminisab.article
+           .select()
+           .where(newminisab.article.categorie == str_categorie))
     for x in cat:
         print(x.title)
     return 'OK'
@@ -112,7 +121,9 @@ def get_categorie_sabnzbd():
         red = None
         try:
             red = redis.StrictRedis(host=host_redis, port=port_redis)
-            categorie_sabnzbd = [x.decode('utf-8') for x in red.lrange('minisab_categorie_sabnzbd', 0, -1)]
+            categorie_sabnzbd = [x.decode('utf-8')
+                                 for x in
+                                 red.lrange(nom_cat_sab, 0, -1)]
         except redis.exceptions.ConnectionError as e:
             logging.error('Impossible de se connecter à Redis : %s', str(e))
     if (len(categorie_sabnzbd) == 0) and (host_sabG is not None):
@@ -127,8 +138,9 @@ def get_categorie_sabnzbd():
         if 'categories' in resultat:
             if (host_redis is not None) and\
                (red is not None):
-                red.lpush('minisab_categorie_sabnzbd', *[x.encode('ascii') for x in resultat['categories']])
-                red.expire('minisab_categorie_sabnzbd', 600)
+                red.lpush(nom_cat_sab, *[x.encode('ascii')
+                                         for x in resultat['categories']])
+                red.expire(nom_cat_sab, 600)
             return resultat['categories']
         else:
             return ''
@@ -177,7 +189,9 @@ def status_sabnzbd():
                     9000)
             r = requests.get(myurl, params=param)
             resultat2 = r.json()
-            return {x['nzo_id']: x['status'] for x in itertools.chain(resultat['history']['slots'], resultat2['queue']['slots'])}
+            resultat_total = itertools.chain(resultat['history']['slots'],
+                                             resultat2['queue']['slots'])
+            return {x['nzo_id']: x['status'] for x in resultat_total}
         except requests.exceptions.ConnectionError:
             return {}
     else:
