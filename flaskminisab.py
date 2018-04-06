7ea8555fdfc09bdf038e336bbd2fc274dc410f9a
GRIMMY <admin@GRIMMY>
2018-04-06 14:37:11 +0200

    2.9b : correction bug sur la recherche qui plante avec des tailles qui contenaient des virgules

--- flaskminisab.py
+++ flaskminisab.py
@@ -14,7 +14,7 @@ filtre_article = [ '*** MOT DE PASSE ***' ]
 logging.config.dictConfig(log_config)
 # logger.setLevel(logging.DEBUG)
 
-version = '2.9a'
+version = '2.9b'
 bp = Blueprint('minisab', __name__, static_folder='/minisab/static')
 app = Flask(__name__)
 logger = app.logger # logging.getLogger('gunicorn.glogging.Logger')
