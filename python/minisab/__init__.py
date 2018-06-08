2cd06ad638483328a66491a934761964d694f774
GuillaumeF <guillaume.frutoso@gmail.com>
2018-06-08 23:04:46 +0200

    formatage de code

--- python/minisab/__init__.py
+++ python/minisab/__init__.py
@@ -1,35 +1,34 @@
-from flask import Flask, Blueprint
-
+from flask import Flask
 import logging
-import os
 
-__version__ = '2.12'
+__version__ = "2.12"
 
-def create_app(mode=''):
-	from . import settings
 
-	logger = logging.getLogger(__name__)
-	if mode == 'dev':
-		logger.setLevel(logging.DEBUG)
-	# handler = logging.FileHandler('/app/minisab.log')
-	# handler.setLevel(logging.DEBUG)
-	# handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s in %(module)s: %(message)s'))
-	# logger.addHandler(handler)
-	handler = logging.StreamHandler()
-	if mode == 'dev':
-		handler.setLevel(logging.DEBUG)
-	handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s in %(module)s: %(message)s'))
-	logger.addHandler(handler)
-	app = Flask(__name__, instance_relative_config=True)
-	settings.init_config(app)
-	
-	from . import flaskminisab
-	app.register_blueprint(flaskminisab.bp, url_prefix='/minisab')
+def create_app(mode=""):
+    from . import settings
 
-	from . import newminisab
-	newminisab.db.init(app.config['MINISAB_AUTRE_DBFILE'])
+    logger = logging.getLogger(__name__)
+    if mode == "dev":
+        logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    if mode == "dev":
+        handler.setLevel(logging.DEBUG)
+    handler.setFormatter(
+        logging.Formatter(
+            "[%(asctime)s] %(levelname)s in %(module)s: %(message)s"
+        )
+    )
+    logger.addHandler(handler)
+    app = Flask(__name__, instance_relative_config=True)
+    settings.init_config(app)
 
-#current_app.config['MINISAB_DBFILE']
-	return app
+    from . import flaskminisab
 
-# app = create_app('dev')
+    app.register_blueprint(flaskminisab.bp, url_prefix="/minisab")
+
+    from . import newminisab
+
+    newminisab.db.init(app.config["MINISAB_AUTRE_DBFILE"])
+
+    return app
+
